7581374ca50fb72ecc7c752691e33344b93f6518
Joe Billingsley <jb@josephbillingsley.co.uk>
2024-10-04 21:40:08 +0100

    Add some notes.

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,12 @@ fn main() {
 
     println!("{0}", ingredients.len());
 
+    // TODO:
+    // [] First try having an algorithm generate some combination of ~40 ingredients of varying portion sizes that meet some calories / carb / etc. target
+    // [] Then could look if enabling recipes with optional values is easier
+    //      [] Might want to split foods into more categories if I go that way
+
     for ingredient in ingredients {
-        println!("{0:?}", ingredient.pack_size)
+        println!("{0:?}", ingredient.name)
     }
 }
